c92d484d85c07b6dc6d5459644cba84cd6d812e6
aravindp98 <83273391+aravindp98@users.noreply.github.com>
2022-03-21 16:02:47 +0530

    Update new2.py

--- new2.py
+++ new2.py
@@ -5,7 +5,7 @@ import hashlib
 
 def hash_file(filename):
    """"This function returns the SHA-1 hash
-   of the file passed into it"""
+   of the file passed into it
 
    # make a hash object
    h = hashlib.sha1()
