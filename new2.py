a358fb8d5b0bc83da4dc5702c0ca87f11c914ca6
aravindp98 <83273391+aravindp98@users.noreply.github.com>
2022-03-23 16:14:06 +0530

    Update new2.py

--- new2.py
+++ new2.py
@@ -15,7 +15,7 @@ def hash_file(filename):
 
        # loop till the end of the file
        chunk = 0 
-       while chunk = b'':
+       while chunk = b:
            # read only 1024 bytes at a time
            chunk = file.read(1024)
            h.update(chunk)
